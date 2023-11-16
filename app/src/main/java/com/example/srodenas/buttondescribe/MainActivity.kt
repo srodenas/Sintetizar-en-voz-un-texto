8244eec0fba463a49b4c7c69d4445ab50e401de3
srodenas <srodher115@g.educaand.es>
2023-11-16 21:13:41 +0100

    Añadimos un progressbar

--- app/src/main/java/com/example/srodenas/buttondescribe/MainActivity.kt
+++ app/src/main/java/com/example/srodenas/buttondescribe/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.srodenas.buttondescribe
 
+import android.os.Binder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.view.LayoutInflater
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
 import com.example.srodenas.buttondescribe.databinding.ActivityMainBinding
 import java.util.Locale
 /*
@@ -22,7 +28,7 @@ tenemos en nuestro dispositivo Local.
 Para ello, nuestro objeto, llama a la función speak, donde le pasamos como parámetro una cadena,
 A ello lo llamamos sintetizar la voz. Existen dos parámetros:
    2.1. QUEVE_FLUSH (Borra la cola y comienza a hablar inmediatamente con el nuevo texto)
-   2.2. QUEVE_ADD (Agrega el texto a la cola y espera su turno para ser hablado. Lo podemos utilzar
+   2.2. QUEVE_ADD (Agrega el texto a la cola y espera su turno para ser hablado. Lo podemos utilizar
    para formar frases.)
    Los otros dos parámetros los dejamos a null.
 
@@ -36,18 +42,41 @@ lateinit, podemos recurrir a verificar si se ha inicializado de la forma anterio
 
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding : ActivityMainBinding
     private lateinit var textToSpeech: TextToSpeech  //descriptor de voz
     private val TOUCH_MAX_TIME = 500 // en milisegundos
-    private var touchLastTime: Long = 0
-    private var touchNumber = 0
+    private var touchLastTime: Long = 0  //para saber el tiempo entre toque.
+    private var touchNumber = 0   //numero de toques dado (por si acaso). De momento no nos hace falta.
+    private lateinit var handler: Handler
+    val MYTAG = "LOGCAT"  //para mirar logs
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        configureTextToSpeech()
-        initEvent()
+        configureTextToSpeech()  //configuramos nuestro textToSpeech
+        initHander()    //lanzaremos un hilo para el progressBar. No es necesario un hilo.
+        initEvent()     //Implementación del botón.
+    }
+
+    private fun initHander() {
+        handler = Handler(Looper.getMainLooper())  //queremos que el tema de la IU, la llevemos al hilo principal.
+        binding.progressBar.visibility = View.VISIBLE  //hacemos visible el progress
+        binding.btnExample.visibility = View.GONE  //ocultamos el botón.
+
+        Thread{
+            Thread.sleep(3000)
+            handler.post{
+                binding.progressBar.visibility = View.GONE  //ocultamos el progress
+                val description = getString(R.string.describe).toString()
+                speakMeDescription(description)  //que nos comente de qué va esto...
+                Thread.sleep(2000)
+                Log.i(MYTAG,"Se ejecuta correctamente el hilo")
+                binding.btnExample.visibility = View.VISIBLE
+
+            }
+        }.start()
     }
 
 
@@ -56,45 +85,50 @@ class MainActivity : AppCompatActivity() {
         textToSpeech = TextToSpeech(applicationContext, TextToSpeech.OnInitListener {
             if(it != TextToSpeech.ERROR){
                 textToSpeech.language = Locale.getDefault()
+               // textToSpeech.setSpeechRate(1.0f)
+                println("Sin problemas en la configuración TextToSpeech")
+            }else{
+                println("Error en la configuración TextToSpeech")
             }
         })
     }
 
 
     private fun initEvent() {
+        val chiste = resources.getString(R.string.chiste)
         binding.btnExample.setOnClickListener{
-
+            //Sacamos el tiempo actual
             val currentTime = System.currentTimeMillis()
+            //Comprobamos si el margen entre pulsación, da lugar a una doble pulsación.
             if (currentTime - touchLastTime < TOUCH_MAX_TIME){
-                touchNumber=0
-                executorDoubleTouch()
-                // println("Ejecuto acción")
+              //  touchNumber=0
+                executorDoubleTouch(chiste)  //hemos pulsado dos veces, por tanto lanzamos el chiste.
+                Log.i(MYTAG,"Escuchamos el chiste")
             }
             else{
-                touchNumber++
-                // println("He pulsado 1 vez")
+              //  touchNumber++
+                Log.i(MYTAG,"Hemos pulsado 1 vez.")
                 //Describimos el botón, 1 sóla pulsación
-                speakMeDescription("Describo el botón")
+                speakMeDescription("Botón para escuchar un chiste")
             }
-
-
-            // val textButton = binding.btnExample.text.toString()
 
             touchLastTime = currentTime
-            if (touchNumber == 2) {
-                println("número de pulsaciones de dos e inicializo")
+          /*  if (touchNumber == 2) {
+                Log.i(MYTAG,"Detectamos 2 pulsaciones.")
                 touchNumber = 0
             }
-
+*/
         }  //fin listener
     }
 
+    //Habla
     private fun speakMeDescription(s: String) {
+        println("Intenta hablar")
         textToSpeech.speak(s, TextToSpeech.QUEUE_FLUSH, null, null)
     }
 
-    private fun executorDoubleTouch() {
-        speakMeDescription("Ejecuto la accion")
+    private fun executorDoubleTouch(chiste: String) {
+        speakMeDescription(chiste)
         // Toast.makeText(this,"doble pulsacion-> Ejecuto la acción",Toast.LENGTH_LONG).show()
     }
 
